96d6212da36b527a12005f7d03cf6f8b959a9d87
jiajianao <2394214767@qq.com>
2022-07-03 19:21:21 +0800

    添加寿命计算

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
-    <input type="text" v-model="num">
-    <button type="button" class="btn btn-primary" @click="num+=3">经验+3</button>
+    <span>我祝你能活</span>
+    <span class="ml-2 mr-2">{{num}}</span>
+    <span>岁</span>
+    <hr>
+    <button type="button" class="btn btn-primary" @click="rand">点击许愿</button>
+    <hr>
+    <button type="button" class="btn btn-success" @click="rands">太少</button>
+    <hr>
+    <span>您可以活</span>
+    <input type="text" v-model="ages" class="put">
+    <span>岁</span>
   </div>
 </template>
 
@@ -11,12 +20,27 @@ export default {
   name: 'App',
   data(){
     return {
-      num:0
+      num:0,
+      ages:0
 
     }
   },
   components: {
     
+  },
+  methods:{
+    rand(){
+      let n = 20
+      let m=60
+      this.num = Math.floor(Math.random()*(m-n+1))+n
+      return this.num
+    },
+      rands(){
+      let n = 70
+      let m=150
+      this.ages = Math.floor(Math.random()*(m-n+1))+n
+      return this.ages
+    }
   }
 }
 </script>
@@ -29,5 +53,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.put{
+  width: 40px;
 }
 </style>
