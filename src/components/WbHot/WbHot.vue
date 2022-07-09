32dd83bf16df1cb4c4598b2bc8f8c6a204c42e9b
jiajianao <2394214767@qq.com>
2022-07-09 20:50:58 +0800

    更新微博图片

--- src/components/WbHot/WbHot.vue
+++ src/components/WbHot/WbHot.vue
@@ -1,59 +1,65 @@
 <template>
-    <div class="hello">
-        <div class="list-group">
-            <div class="list-group-item list-group-item-action">
-              <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1 t">{{title}}</h5>
-              </div>
-              <span class="badge badge-primary badge-pill hot">热度:{{hot}}</span>
-            </div>
-          </div>
-  
+  <div class="hello" style="background-color: #eeffff">
+    <div class="list-group">
+      <div class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1 t"><span style="color:#f26d5f ">{{indexs}}</span>：{{title}}</h5>
+        </div>
+        <span class="badge badge-success badge-pill hot">热度:{{hot}}</span>
+      </div>
     </div>
-  </template>
-  
-  <script>
+
+  </div>
+</template>
+
+<script>
   export default {
     name: 'WbHot',
-  
+
     props: {
-      title:{
-        type:String,
-        default:''
+      indexs:{
+        type:Number,
+        default:0
       },
-      hot:{
-        type:String,
-        default:''
+      title: {
+        type: String,
+        default: ''
       },
-      url:{
-        type:String,
-        default:''
+      hot: {
+        type: String,
+        default: ''
       },
-      time:{
-        type:String,
-        default:'00:00:00'
+      url: {
+        type: String,
+        default: ''
+      },
+      time: {
+        type: String,
+        default: '00:00:00'
       }
     },
-    methods:{
-  
+    methods: {
+
     }
   }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  .urlSize{
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .urlSize {
     font-size: 1px
-    /* margin-left:0 */
+      /* margin-left:0 */
   }
-  .badge{
+
+  .badge {
     line-height: 1.8
   }
-  .t{
-    text-align: center
+
+  .t {
+    text-align: left
   }
-  .hot{
+
+  .hot {
     float: right;
   }
-  </style>
-  
+</style>
